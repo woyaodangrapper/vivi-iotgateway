99e29e1701eb2ba64d5da311881b5c80d594b4dc
BUG killer：） <38523332+woyaodangrapper@users.noreply.github.com>
2025-04-13 13:07:27 +0800

    chore: update license statement to avoid copyright disputes

--- src/Application Core/Domain/Core/Vivi.Dcs.Contracts/Commands/Device/QueryDeviceCommand.cs
+++ src/Application Core/Domain/Core/Vivi.Dcs.Contracts/Commands/Device/QueryDeviceCommand.cs
@@ -2,22 +2,26 @@
 
 public class QueryDeviceCommand : SearchPagedDTO
 {
-    public string? name { get; set; }
-    public short? model { get; set; }
-    public string? number { get; set; }
-    public short? status { get; set; }
-    public DateTime? startTime { get; set; }
-    public DateTime? endTime { get; set; }
+  public string? name { get; set; }
+  public short? model { get; set; }
+  public string? number { get; set; }
+  public short? status { get; set; }
+  public DateTime? startTime { get; set; }
+  public DateTime? endTime { get; set; }
 
-    public DateTime? FormattedStartTime
-    {
-        get => startTime?.Date;// 取开始时间的日期部分，即当天的凌晨时间
-        set => startTime = value.HasValue && startTime.HasValue ? value.Value.Date + startTime.Value.TimeOfDay : value;//  将开始时间的日期部分替换为指定日期，时间部分不变 如果 value 为 null，则直接将 startTime 赋值为 null
-    }
+  public DateTime? FormattedStartTime
+  {
+    // 取开始时间的日期部分，即当天的凌晨时间
+    get => startTime?.Date;
+    //  将开始时间的日期部分替换为指定日期，时间部分不变 如果 value 为 null，则直接将 startTime 赋值为 null
+    set => startTime = value.HasValue && startTime.HasValue ? value.Value.Date + startTime.Value.TimeOfDay : value;
+  }
 
-    public DateTime? FormattedEndTime
-    {
-        get => endTime?.Date.AddDays(1).AddTicks(-1); // 取结束时间的日期部分，加上一天再减去一刻钟的时间，即当天的最后一刻钟时间
-        set => endTime = value.HasValue && endTime.HasValue ? value.Value.Date + endTime.Value.TimeOfDay : value;// 将结束时间的日期部分替换为指定日期，时间部分不变 如果 value 为 null，则直接将 endTime 赋值为 null
-    }
+  public DateTime? FormattedEndTime
+  {
+    // 取结束时间的日期部分，加上一天再减去一刻钟的时间，即当天的最后一刻钟时间
+    get => endTime?.Date.AddDays(1).AddTicks(-1);
+    // 将结束时间的日期部分替换为指定日期，时间部分不变 如果 value 为 null，则直接将 endTime 赋值为 null
+    set => endTime = value.HasValue && endTime.HasValue ? value.Value.Date + endTime.Value.TimeOfDay : value;
+  }
 }
